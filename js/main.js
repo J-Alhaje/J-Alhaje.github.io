4984ba16c7a079fe5758956a114a4f9d93f36d4c
Jamil Alhaje <302316781@student.rocmondriaan.nl>
2023-06-02 21:41:56 +0200

    Ik heb de navbar gemaakt

--- js/main.js
+++ js/main.js
@@ -1,20 +1,19 @@
 $(document).ready(function () {
-
-    $('.first-button').on('click', function () {
-  
-      $('.animated-icon1').toggleClass('open');
-    });
-    $('.second-button').on('click', function () {
-  
-      $('.animated-icon2').toggleClass('open');
-    });
-    $('.third-button').on('click', function () {
-  
-      $('.animated-icon3').toggleClass('open');
-    });
+  $(".first-button").on("click", function () {
+    $(".animated-icon1").toggleClass("open");
   });
+  $(".second-button").on("click", function () {
+    $(".animated-icon2").toggleClass("open");
+  });
+  $(".third-button").on("click", function () {
+    $(".animated-icon3").toggleClass("open");
+  });
+});
 
+// Define a function to be executed after a delay
+function myFunction() {
+  console.log("Hello, world!");
+}
 
-setTimeout(function (){
-  console.log()
-},1000("timer in seconden"))
+// Call setTimeout() and pass in the function to be executed, along with the delay in milliseconds
+setTimeout(myFunction, 01, 200, 2000); // This will execute myFunction after 1 second (1000 milliseconds)
