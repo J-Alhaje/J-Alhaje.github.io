fb49d489a3f95c23e356adbb31995c6efcf4c91c
Jamil Alhaje <302316781@student.rocmondriaan.nl>
2023-06-03 03:30:45 +0200

    Ik ben bezig met navbar

--- js/main.js
+++ js/main.js
@@ -1,19 +1,6 @@
-$(document).ready(function () {
-  $(".first-button").on("click", function () {
-    $(".animated-icon1").toggleClass("open");
-  });
-  $(".second-button").on("click", function () {
-    $(".animated-icon2").toggleClass("open");
-  });
-  $(".third-button").on("click", function () {
-    $(".animated-icon3").toggleClass("open");
-  });
-});
+const img = document.querySelector("#img");
+const input = document.querySelector("#input");
 
-// Define a function to be executed after a delay
-function myFunction() {
-  console.log("Hello, world!");
-}
-
-// Call setTimeout() and pass in the function to be executed, along with the delay in milliseconds
-setTimeout(myFunction, 01, 200, 2000); // This will execute myFunction after 1 second (1000 milliseconds)
+input.onchange = () => {
+  if (input.files[0]) img.src = URL.createObjectURL(input.files[0]);
+};
