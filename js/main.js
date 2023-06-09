e588fbab30c7cd34f945b76212d4f0c8ff2aa128
Jamil Alhaje <302316781@student.rocmondriaan.nl>
2023-06-09 14:32:33 +0200

    Ik heb de navbar responsive gemaakt

--- js/main.js
+++
@@ -1,6 +0,0 @@
-const img = document.querySelector("#img");
-const input = document.querySelector("#input");
-
-input.onchange = () => {
-  if (input.files[0]) img.src = URL.createObjectURL(input.files[0]);
-};
