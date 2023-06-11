af13c7025e4896b57f9c9359e0661d01c1e7b437
Jamil Alhaje <302316781@student.rocmondriaan.nl>
2023-06-11 19:07:55 +0200

    Ik heb de about-me section afgemaakt

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// Vue.js vaardigheden
+const vueSkills = [
+  "Begrip van Vue.js voor het bouwen van interactieve webinterfaces",
+  "Creëren en beheren van Vue-componenten",
+  "Bidirectionele gegevensbinding en synchronisatie",
+  "Gebruik van Vue-directieven voor dynamische weergave en gegevensbinding",
+  "Afhandelen van gebeurtenissen met Vue",
+  "Component-communicatie met props en custom events",
+  "Kennis van Vue-component levenscyclusmethoden",
+  "Implementatie van routing met Vue Router",
+  "Basisvaardigheden in VueX voor state management",
+];
+
+// Uitvoer van Vue.js vaardigheden
+console.log("Mijn Vue.js vaardigheden omvatten:");
+vueSkills.forEach((skill, index) => {
+  console.log(`${index + 1}. ${skill}`);
+});
